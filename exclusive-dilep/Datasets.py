f93a3048b52534691ae00eee5b2e29cc805af053
Michael Pitt <mpitt@lxplus766.cern.ch>
2021-08-26 13:15:50 +0200

    prepare di-lep example

--- exclusive-dilep/Datasets.py
+++ exclusive-dilep/Datasets.py
@@ -3,50 +3,41 @@
 import os
 
 datasets={
-    'DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8':{
-        'v2' : ['RunIISummer20UL18NanoAODv2','Pilot_106X_upgrade2018_realistic_v15_L1v1-v2'],
-        'v9' : ['RunIISummer20UL18NanoAODv9','Pilot_106X_upgrade2018_realistic_v15_L1v1-v2'],
-    },
-    'DYToMuMu_pomflux_Pt-30_TuneCP5_13TeV-pythia8' : {
-        'v2' : ['RunIISummer20UL18NanoAODv2','106X_upgrade2018_realistic_v15_L1v1-v1'],
-        'v9' : ['RunIISummer20UL18NanoAODv9','106X_upgrade2018_realistic_v16_L1v1-v1'],
-    }
+    'DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8':['RunIISummer20UL18NanoAODv9','Pilot_106X_upgrade2018_realistic_v15_L1v1-v2'],
+    'DYToMuMu_pomflux_Pt-30_TuneCP5_13TeV-pythia8' : ['RunIISummer20UL18NanoAODv9','106X_upgrade2018_realistic_v16_L1v1-v1'],
 }
-for era in ['A','B','C','D','E','F']:
-    for stream in ['SingleMuon','DoubleMuon','EGamma']:
-        datasets['Run2017%s_%s'%(era,stream)]={
-            'v2' : ['UL2017_MiniAODv1_NanoAODv2-v1'],
-            'v9' : ['UL2017_MiniAODv2_NanoAODv9-v1']
-        }
 
-for era in ['A','B','C','D']:
-    for stream in ['SingleMuon','DoubleMuon','EGamma']:
-        datasets['Run2018%s_%s'%(era,stream)]={
-            'v2' : ['UL2018_MiniAODv1_NanoAODv2-v1'],
-            'v9' : ['UL2018_MiniAODv2_NanoAODv9-v1']
-        }
+# 2017 datasets
+for stream in ['SingleMuon','DoubleMuon','SingleElectron']:
+    for era in ['A','B','C','D','E','F']:
+        datasets['Run2017%s_%s'%(era,stream)]=['UL2017_MiniAODv2_NanoAODv9-v1']
+    
+#2018 datasets
+for stream in ['SingleMuon','DoubleMuon','EGamma']:
+    for era in ['A','B','C','D']:
+        datasets['Run2018%s_%s'%(era,stream)]=['U2018_MiniAODv2_NanoAODv9-v1']
 
 #corrections:
 
 
-def ConvertToDASds(ds, v='v9'):
+def ConvertToDASds(ds):
     if 'Run' in ds:
-        return '/'+ds.split('_')[1]+'/'+('-'.join([ds.split('_')[0],datasets[ds][v][0]]))+'/NANOAOD'
-    return '/'+ds+'/'+('-'.join(datasets[ds][v]))+'/NANOAODSIM'
+        return '/'+ds.split('_')[1]+'/'+('-'.join([ds.split('_')[0],datasets[ds][0]]))+'/NANOAOD'
+    return '/'+ds+'/'+('-'.join(datasets[ds]))+'/NANOAODSIM'
 
-def ConvertToEOSDirectory(ds, v='v9'):
+def ConvertToEOSDirectory(ds):
     if 'Run' not in ds:
-        return '/eos/cms/store/mc/'+datasets[ds][v][0]+'/'+ds+'/NANOAODSIM/'+datasets[ds][v][1]
-    return '/eos/cms/store/data/'+ds.replace('_','/')+'/NANOAOD/'+datasets[ds][v][0]
+        return '/eos/cms/store/mc/'+datasets[ds][0]+'/'+ds+'/NANOAODSIM/'+datasets[ds][1]
+    return '/eos/cms/store/data/'+ds.replace('_','/')+'/NANOAOD/'+datasets[ds][0]
 
 
-def listDatasets(v='v9'):
+def listDatasets():
     
     """ prints the available keys """
 
     print('The following datasets are available:')
     for ds in datasets.keys():
-        fileList=getFilesForDataset(ds,v=v,eos=True)
+        fileList=getFilesForDataset(ds,eos=True)
         size=0; unit='bytes'
         for file in fileList: size+=os.path.getsize(file) 
         if (size // 1000) > 0: size/=1000; unit='kB'
@@ -57,7 +48,7 @@ def listDatasets(v='v9'):
         msg='(No directories available in EOS for this dataset)'
         if nfiles:
             msg=('(in EOS: nfiles = %d, size = %2.2f%s)'%(nfiles,size,unit))
-        print(ConvertToDASds(ds=ds,v=v),msg)
+        print(ConvertToDASds(ds=ds),msg)
     return
 
 def getFilesFor(sample, eos=True):
@@ -81,7 +72,7 @@ def getFilesFor(sample, eos=True):
     
     return fileList
 
-def getFilesForDataset(dataset, v='v9', eos=True):
+def getFilesForDataset(dataset, eos=True):
 
     """ lists the files in a dataset directory and retrieves a list of files for analysis """
 
@@ -92,7 +83,7 @@ def getFilesForDataset(dataset, v='v9', eos=True):
         print('ERROR: Unknown dataset - '+dataset)
         return []
     
-    sample_dir=ConvertToEOSDirectory(dataset, v)
+    sample_dir=ConvertToEOSDirectory(dataset)
     fileList=getFilesFor(sample_dir, eos=eos)
     
     return fileList
